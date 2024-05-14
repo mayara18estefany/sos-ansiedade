433dd36d7c5b62737808460e462e44dc929403ab
mayara18estefany <tefynha7081@gmail.com>
2024-05-14 19:25:53 -0300

    PI

---
+++ app/src/main/java/com/example/telapi/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.telapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageServe;
+    private ImageView imageMM;
+    private ImageView imageSos;
+    private  ImageView imageSa;
+    private ImageView imageExe;
+    private ImageView imageFeed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageServe= findViewById(R.id.imageServe);
+        imageMM = findViewById(R.id.imageMM);
+        imageSos = findViewById(R.id.imageSos);
+        imageSa = findViewById(R.id.imageSa);
+        imageExe = findViewById(R.id.imageExe);
+        imageFeed = findViewById(R.id.imageFeed);
+
+        imageServe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), resul.class);
+
+                startActivity(intent);
+            }
+        }
+        );
+        imageMM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), mensagens.class);
+
+                startActivity(intent);
+            }
+        });
+
+        imageSos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), sos.class);
+
+                startActivity(intent);
+            }
+        });
+
+        imageSa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Sobre.class);
+
+                startActivity(intent);
+            }
+        });
+
+        imageExe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), exercicio.class);
+
+                startActivity(intent);
+            }
+        });
+
+        imageFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Feed.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}

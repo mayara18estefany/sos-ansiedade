ed588a1b3fcc437a1399d0cc1219720f5ed433d8
Andrey5643 <andreycavalcanti.01tb@gmail.com>
2024-05-29 20:15:38 -0300

    Mensagens

--- app/src/main/java/com/example/telapi/mensagens.java
+++ app/src/main/java/com/example/telapi/mensagens.java
@@ -2,13 +2,30 @@ package com.example.telapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class mensagens extends AppCompatActivity {
+    private ImageView imageVoltarFe;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagens);
+
+        imageVoltarFe = findViewById(R.id.imageVoltarFe);
+
+        imageVoltarFe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
